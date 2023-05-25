3832d22d3ce88e839a76bbddb3db7672ee3f92d2
alexsavinov <spell477@gmail.com>
2023-05-25 13:38:25 +0300

    fixes

--- core/src/main/java/org/example/Main.java
+++ core/src/main/java/org/example/Main.java
@@ -2,9 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-//        StringUtils stringUtils = new StringUtils();
         System.out.println(new StringBuilder("111 ").append(StringUtils.isPositiveNumber("111")));
-//        System.out.println(new StringBuilder("-33 ").append(StringUtils.isPositiveNumber("-33")));
-//        System.out.println(new StringBuilder("0 ").append(StringUtils.isPositiveNumber("0")));
     }
 }
--- core/src/main/java/org/example/Utils.java
+++ core/src/main/java/org/example/Utils.java
@@ -1,22 +1,14 @@
 package org.example;
 
 public class Utils {
+
     public static boolean isAllPositiveNumbers(String... str) {
         for (String s : str) {
-//            return StringUtils.isPositiveNumber(s);
-//            System.out.println(s);
-//            new utils;
-//            StringUtils utils = new StringUtils();
-//            StringUtils stringUtils = new StringUtils();
-//            StringUtils stringUtils = new StringUtils();
             if (!StringUtils.isPositiveNumber(s)) {
                 return false;
             }
-
-//            new utils
-
         }
-//        System.out.println(str.length);
+
         return true;
     }
 }
